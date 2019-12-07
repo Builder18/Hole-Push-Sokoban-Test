c426ba2748cc2b3e20a9b754b69d13c560496411
Builder18 <36567146+Builder18@users.noreply.github.com>
2019-12-07 10:30:03 +0200

    Update GameBar.js

--- Hole Push Sokoban/GameBar.js
+++ Hole Push Sokoban/GameBar.js
@@ -114,6 +114,6 @@ DelayUntil(C,GameBarLoad);
 //Check everything loaded properly after 10 seconds
 function NotAllLoaded(){
 	if(!CC()||puzzlescriptModules.map(function(m){return !precedences[m]()}).some(Identity))
-		alert("Not all game bar modules loaded properly. Please file an issue at: https://github.com/pedropsi/game-bar-source/issues");
+		alert("Not all game bar modules loaded properly. Please allow pedropsi.github.io on your script blocker.");
 }
 setTimeout(NotAllLoaded,10000);
