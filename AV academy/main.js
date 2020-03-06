91a1cedd9afedec9cef63247faa5f205396ded8e
Builder18 <36567146+Builder18@users.noreply.github.com>
2020-03-06 15:20:30 +0200

    Dialog created

--- AV academy/main.js
+++ AV academy/main.js
@@ -2,21 +2,37 @@
 
 var textNumber = 1;
 
-document.getElementById('sb-image').addEventListener('click', function(){
+var sb_image = document.getElementById('sb_image');
+var sb_text = document.getElementById('sb_text');
+var defenderImage = document.getElementById('defenderImage');
+
+sb_image.addEventListener('click', function(){
 	textNumber++;
 	console.log(textNumber);
+	if (textNumber === 2) {
+		sb_text.textContent = 'Your first lesson is to find your installation folder and open configuration file in there.';
+	}
+	if (textNumber === 3) {
+		sb_image.style.display = "none";
+		sb_text.style.display = "none";
+		defenderImage.style.display = "none";
+	}
 });
 
-document.getElementById('sb-text').addEventListener('click', function(){
+sb_text.addEventListener('click', function(){
 	textNumber++;
 	console.log(textNumber);
+	if (textNumber === 2) {
+		sb_text.textContent = 'Your first lesson is to find your installation folder and open configuration file in there.';
+	}
+	if (textNumber === 3) {
+		sb_image.style.display = "none";
+		sb_text.style.display = "none";
+		defenderImage.style.display = "none";
+	}
 });
 
-/*function next() {
-	
-}*/
-
-function tickle() {
+defenderImage.addEventListener('click', function(){
 	alert('Ahhahaa! Don\'t tickle!');
-}
+});
 
