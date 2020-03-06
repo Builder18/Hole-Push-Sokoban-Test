1b5873c1398751de2a7b68029126a6f1b7c9128c
Builder18 <36567146+Builder18@users.noreply.github.com>
2020-03-06 14:16:27 +0200

    Javascript output testing

--- AV academy/main.js
+++ AV academy/main.js
@@ -1,1 +1,22 @@
-//placeholder
+
+
+var textNumber = 1;
+
+document.getElementById('sb-image').addEventListener('click', function(){
+	textNumber++;
+	console.log(textNumber);
+});
+
+document.getElementById('sb-text').addEventListener('click', function(){
+	textNumber++;
+	console.log(textNumber);
+});
+
+/*function next() {
+	
+}*/
+
+function tickle() {
+	alert('Ahhahaa! Don\'t tickle!');
+}
+
