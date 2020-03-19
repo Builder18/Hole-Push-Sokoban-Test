6f63b3c09b1a47a7cf67c894b461958aaff73455
Builder18 <36567146+Builder18@users.noreply.github.com>
2020-03-19 08:05:11 +0200

    Folder testing

--- AV academy/main.js
+++ AV academy/main.js
@@ -1,10 +1,35 @@
 
-
+//Variables that can change go here
 var textNumber = 1;
 
+//Elements go here
 var sb_image = document.getElementById('sb_image');
 var sb_text = document.getElementById('sb_text');
 var defenderImage = document.getElementById('defenderImage');
+var defenderLogo = document.getElementById('defenderLogo');
+var configLogo = document.getElementById('configLogo');
+var programFilesImage = document.getElementById('programFilesImage');
+var windowsImage = document.getElementById('windowsImage');
+var usersImage = document.getElementById('usersImage');
+
+//Hiding elements that aren't needed in beginning
+defenderLogo.style.display = "none";
+configLogo.style.display = "none";
+programFilesImage.style.display = "none";
+windowsImage.style.display = "none";
+usersImage.style.display = "none";
+
+programFilesImage.addEventListener('click', function(){
+	alert('Place for installed programs.');
+});
+
+windowsImage.addEventListener('click', function(){
+	alert('Access denied!');
+});
+
+usersImage.addEventListener('click', function(){
+	alert('I, you, me.');
+});
 
 sb_image.addEventListener('click', function(){
 	textNumber++;
@@ -16,6 +41,9 @@ sb_image.addEventListener('click', function(){
 		sb_image.style.display = "none";
 		sb_text.style.display = "none";
 		defenderImage.style.display = "none";
+		programFilesImage.style.display = "inline";
+		windowsImage.style.display = "inline";
+		usersImage.style.display = "inline";
 	}
 });
 
@@ -29,6 +57,9 @@ sb_text.addEventListener('click', function(){
 		sb_image.style.display = "none";
 		sb_text.style.display = "none";
 		defenderImage.style.display = "none";
+		programFilesImage.style.display = "inline";
+		windowsImage.style.display = "inline";
+		usersImage.style.display = "inline";
 	}
 });
 
