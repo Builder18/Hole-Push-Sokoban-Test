145d48757699726fb15b7440a134020ad8071955
Builder18 <36567146+Builder18@users.noreply.github.com>
2020-05-03 13:53:15 +0300

    Timebender and some minor changes

--- Lord of time/script.js
+++ Lord of time/script.js
@@ -9,10 +9,12 @@ var gameData = {
 	gathererPrice: 11,
 	currentSandGatherers: 0,
 	currentHourglass: 0,
+	hasTimeBender: 0,
 };
 
 //No need to save these variables
 var ctime;
+var num;
 
 //Elements go here
 var time = document.getElementById('time');
@@ -25,10 +27,13 @@ var sandGatherers = document.getElementById('sandGatherers');
 var gathererSpan = document.getElementById('gathererSpan');
 var hourglassSpan = document.getElementById('hourglassSpan');
 var btnHourglass = document.getElementById('btnHourglass');
+var timebenderSpan = document.getElementById('timebenderSpan');
+var btnTimeBender = document.getElementById('btnTimeBender');
 
 //Hiding elements that aren't needed in beginning
 sandUpgrades.style.display = 'none';
 hourglassSpan.style.display = 'none';
+timebenderSpan.style.display = 'none';
 
 //Listeners and other code go here
 btnSand.addEventListener("mouseover", SandOver);
@@ -46,6 +51,10 @@ btnSandGatherer.addEventListener("click", SandGatherer);
 btnHourglass.addEventListener("mouseover", HourglassOver);
 btnHourglass.addEventListener("mouseout", HourglassOut);
 btnHourglass.addEventListener("click", Hourglass);
+//Time bender
+btnTimeBender.addEventListener("mouseover", TimeBenderOver);
+btnTimeBender.addEventListener("mouseout", TimeBenderOut);
+btnTimeBender.addEventListener("click", TimeBender);
 
 function SandOver() {
   SandTimeout = setInterval(SandIncrease, 1000);
@@ -79,24 +88,36 @@ function HourglassOut() {
   clearInterval(HourglassTimeout);
 }
 
+function TimeBenderOver() {
+  TimeBenderTimeout = setInterval(TimeBender, 1000);
+}
+
+function TimeBenderOut() {
+  clearInterval(TimeBenderTimeout);
+}
+
 //After timeout handlers, main functionality of buttons goes here
 
 function SandIncrease() {
 	gameData.currentSand++;
 	sand.innerHTML = gameData.currentSand;
-	if (gameData.currentSand === 11) sandUpgrades.style.display = 'block';
+	if (gameData.currentSand === 11) {
+		if (gameData.hasSandUpgrade === 0) gameData.hasSandUpgrade = 1;
+		sandUpgrades.style.display = 'block';
+	}
 }
 
 function TimeIncrease() {
-	if (gameData.currentSand < (2 * gameData.timeMultiplier)) return;
+	//Let's try with multiplier of 10 instead of 5 for sand
+	if (gameData.currentSand < (10 * gameData.timeMultiplier)) return;
 	//If we have enough sand, increase time with 5*timeMultiplier, decrease sand with 2*timeMultiplier and upgrade html for both of them.
 	gameData.currentTime += 5 * gameData.timeMultiplier;
 	time.innerHTML = 	fancyTimeFormat(gameData.currentTime);
-	gameData.currentSand -= 2 * gameData.timeMultiplier;
-	sand.innerHTML = gameData.currentSand;
+	gameData.currentSand -= 10 * gameData.timeMultiplier;
+	sand.innerHTML = thousands_separators(gameData.currentSand);
 	//Increase timeMultiplier and change button text
 	gameData.timeMultiplier++;
-	btnTime.innerHTML = (2 * gameData.timeMultiplier) + " sand";
+	btnTime.innerHTML = (10 * gameData.timeMultiplier) + " sand";
 }
 
 function SandGatherer() {
@@ -104,25 +125,42 @@ function SandGatherer() {
 	//If enough sand, increase sand gatherers with 1, decrease sand with 11 (this doesn't increase)
 	gameData.currentSandGatherers++;
 	if (gameData.currentSandGatherers === 2) btnSand.style.display = 'none';
+	if (gameData.currentSandGatherers === 50) {
+		gameData.gathererPrice = 50;
+		btnSandGatherer.innerHTML = 50 + " sand";
+	}
 	if (gameData.currentSandGatherers === 100) {
 		gathererSpan.style.display = 'none';
 		hourglassSpan.style.display = 'inline';
+		timebenderSpan.style.display = 'inline';
+		if (gameData.hasSandUpgrade === 1) gameData.hasSandUpgrade = 2;
 	} 
 	sandGatherers.innerHTML = gameData.currentSandGatherers;
-	gameData.currentSand -= 11;
-	sand.innerHTML = gameData.currentSand;
+	gameData.currentSand -= gameData.gathererPrice;
+	sand.innerHTML = thousands_separators(gameData.currentSand);
 }
 
 function Hourglass() {
 	if (gameData.currentSand < 900 * (gameData.currentHourglass + 1)) return;
 	//If enough sand, increase sand gatherers with 1, decrease sand with 11 (this doesn't increase)
 	gameData.currentHourglass++;
-	if (gameData.currentHourglass === 100) {
-		hourglassSpan.style.display = 'none';
-	} 
 	gameData.currentSand -= 900 * gameData.currentHourglass;
+	sand.innerHTML = thousands_separators(gameData.currentSand);
+	btnHourglass.innerHTML = thousands_separators(900 * (gameData.currentHourglass + 1)) + " sand";
+}
+
+function TimeBender() {
+	if (gameData.currentSand < 9999) return;
+	//If enough sand, enable time bender and hide it...
+	gameData.hasTimeBender = 1;
+	timebenderSpan.style.display = 'none';
+		//autoHourglassSpan.style.display = 'inline';
+		//timebenderSpan.style.display = 'inline';
+	if (gameData.hasSandUpgrade === 2) gameData.hasSandUpgrade = 3;
+	//Reduce sand
+	gameData.currentSand -= 9999;
 	sand.innerHTML = gameData.currentSand;
-	btnHourglass.innerHTML = (900 * (gameData.currentHourglass + 1)) + " sand";
+	timeBenderLoop();
 }
 
 function fancyTimeFormat(ctime)
@@ -144,19 +182,36 @@ function fancyTimeFormat(ctime)
     return ret;
 }
 
+function thousands_separators(num)
+  {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
+
+//Time bender increases time slowly
+function timeBenderLoop() {
+	gameData.currentTime++;
+	gameData.currentSand -= 100;
+	time.innerHTML = fancyTimeFormat(gameData.currentTime);
+	sand.innerHTML = thousands_separators(gameData.currentSand);
+	//timeBenderLoop runs every three seconds
+	setTimeout(timeBenderLoop, 3000);
+}
+
 //Game loop
 function gameLoop() {
 	gameData.currentTime--;
-	time.innerHTML = 	fancyTimeFormat(gameData.currentTime);
+	time.innerHTML = fancyTimeFormat(gameData.currentTime);
 	//time.innerHTML = gameData.currentTime;
-	gameData.currentSand += gameData.currentSandGatherers;
-	sand.innerHTML = gameData.currentSand;
+	gameData.currentSand += gameData.currentSandGatherers * (gameData.currentHourglass + 1);
+	sand.innerHTML = thousands_separators(gameData.currentSand);
 	//If time runs out, reset time, sand and HTML for them
 	if (gameData.currentTime < 1) {
 		gameData.currentTime = 10;
-		time.innerHTML = 	fancyTimeFormat(gameData.currentTime);
+		time.innerHTML = fancyTimeFormat(gameData.currentTime);
 		gameData.currentSand = 0;
-		sand.innerHTML = gameData.currentSand;
+		sand.innerHTML = thousands_separators(gameData.currentSand);
 	}
 	//gameLoop runs every second
 	setTimeout(gameLoop, 1000);
