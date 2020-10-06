f6ed3a8022016b0b51edb4e573c422f098bf4ca5
Builder18 <36567146+Builder18@users.noreply.github.com>
2020-10-06 11:13:23 +0300

    Progress mashup up

---
+++ metagames/README.md
@@ -0,0 +1,5 @@
+This folder is supposed to have some metagames, or games with unusual mechanics.
+
+List of metagames in this folder:
+
+- 
---
+++ metagames/progress-mashup/progress-mashup.js
@@ -0,0 +1,126 @@
+(function() {
+	//Trying strict mode, remove if required
+	"use strict"
+//Variables that can change go here
+var gameData = {
+	currentProgress: 1,
+	m1IncreaseVar: 0,
+	m2IncreaseVar: 0,
+	m3IncreaseVar: 0,
+};
+
+//Progress bar variables
+var width = 1;
+var width2 = 1;
+var width3 = 1;
+
+var ctime, num;
+
+//Elements go here
+var bigProgress = document.getElementById('myBar');
+var m1Bar = document.getElementById('m1Bar');
+var m2Bar = document.getElementById('m2Bar');
+var m3Bar = document.getElementById('m3Bar');
+var m1Span = document.getElementById('m1Progress');
+var m2Span = document.getElementById('m2Progress');
+var m3Span = document.getElementById('m3Progress');
+
+//Hiding elements that aren't needed in beginning
+//m4Span.style.display = 'none';
+//m5Span.style.display = 'none';
+
+//Listeners and other code go here
+m1Span.addEventListener("click", m1Increase);
+m2Span.addEventListener("click", m2Increase);
+m3Span.addEventListener("click", m3Increase);
+
+//Main functionality of buttons goes here
+
+function m1Increase() {
+	gameData.m1IncreaseVar++;
+	//console.log("m1 clicked!");
+}
+
+function m2Increase() {
+	gameData.m2IncreaseVar++;
+	//console.log("m2 clicked!");
+}
+
+function m3Increase() {
+	gameData.m3IncreaseVar++;
+	//console.log("m3 clicked!");
+}
+
+function fancyTimeFormat(ctime)
+{   
+    // Hours, minutes and seconds
+    var hrs = ~~(ctime / 3600);
+    var mins = ~~((ctime % 3600) / 60);
+    var secs = ~~ctime % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
+function thousands_separators(num)
+  {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
+  
+//Game loop
+function gameLoop() {
+	//Progress bar for m1
+	
+	if (width >= 100) {
+		 gameData.m1IncreaseVar = 0;
+		 gameData.currentProgress++;
+		 bigProgress.style.width = gameData.currentProgress + "%";
+		 m1Bar.style.width = "1%";
+		 width = 1;
+   } else {
+		 width += gameData.m1IncreaseVar;
+		 m1Bar.style.width = width + "%";
+    }
+	 
+	//Once m1 is working, repeat this for m2 and m3
+	 
+	if (width2 >= 100) {
+		 gameData.m2IncreaseVar = 0;
+		 gameData.currentProgress++;
+		 bigProgress.style.width = gameData.currentProgress + "%";
+		 m2Bar.style.width = "1%";
+		 width2 = 1;
+   } else {
+		 width2 += gameData.m2IncreaseVar;
+		 m2Bar.style.width = width2 + "%";
+    }
+	 
+	 //And m3
+	 
+	 if (width3 >= 100) {
+		 gameData.m3IncreaseVar = 0;
+		 gameData.currentProgress++;
+		 bigProgress.style.width = gameData.currentProgress + "%";
+		 m3Bar.style.width = "1%";
+		 width3 = 1;
+   } else {
+		 width3 += gameData.m3IncreaseVar;
+		 m3Bar.style.width = width3 + "%";
+    }
+	
+	//gameLoop runs every second
+	setTimeout(gameLoop, 1000);
+}
+
+gameLoop();
+})();
