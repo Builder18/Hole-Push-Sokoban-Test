55467a24eaf1d101fea87843601fdb1fe221248e
Builder18 <36567146+Builder18@users.noreply.github.com>
2020-11-27 20:22:32 +0200

    Add files via upload

--- metagames/README.md
+++ metagames/README.md
@@ -3,3 +3,5 @@ This folder is supposed to have some metagames, or games with unusual mechanics.
 List of metagames in this folder:
 
 - Progress mashup: https://builder18.github.io/metagames/progress-mashup/progress-mashup.html
+
+- Word incremental: https://builder18.github.io/metagames/word-incremental/word-incremental.html
---
+++ metagames/word-incremental/word-incremental.js
@@ -0,0 +1,92 @@
+(function() {
+	//Trying strict mode, remove if required
+	"use strict"
+//Variables that can change go here
+var gameData = {
+	currentWords: 1,
+	currentSentences: 0,
+	
+};
+
+var ctime, num;
+
+//Elements go here
+var sentenceSpan = document.getElementById('sentenceSpan');
+var currentString = document.getElementById('currentString');
+
+//Hiding elements that aren't needed in beginning
+//m4Span.style.display = 'none';
+//m5Span.style.display = 'none';
+
+//Listeners and other code go here
+//window.addEventListener("click", wordIncrease);
+
+//Main functionality of buttons goes here
+
+function letterClicked() {
+	
+}
+
+//Still unsure how to make string with letters that you click on...
+
+function wordIncrease() {
+		if (gameData.currentWords > 100) {
+			gameData.currentSentences++;
+			sentenceSpan.textContent = gameData.currentSentences;
+			gameData.currentWords = 0;
+			document.body.style.backgroundSize = "1%";
+		}
+		gameData.currentWords += 10;
+		document.body.style.backgroundSize = gameData.currentWords + "%";
+}
+
+function fancyTimeFormat(ctime)
+{   
+    // Hours, minutes and seconds
+    var hrs = ~~(ctime / 3600);
+    var mins = ~~((ctime % 3600) / 60);
+    var secs = ~~ctime % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
+function thousands_separators(num)
+  {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
+  
+//Game loop
+function gameLoop() {
+	//Progress bar for m1
+	
+	/*if (width >= 100) {
+		 gameData.m1IncreaseVar++;
+		 m1Idle.textContent = gameData.m1IncreaseVar;
+		 gameData.currentProgress++;
+		 bigProgress.style.width = gameData.currentProgress + "%";
+		 m1Bar.style.width = "1%";
+		 width = 1;
+   } else {
+		 width += gameData.m1IncreaseVar;
+		 m1Bar.style.width = width + "%";
+    }*/
+	 
+	 
+	
+	//gameLoop runs every second
+	setTimeout(gameLoop, 1000);
+}
+
+gameLoop();
+})();
